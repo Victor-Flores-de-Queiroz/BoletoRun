95503ce86009b747878d949f74ee39fa1628a6a9
Victor-Flores-de-Queiroz <victorflores98.vf@gmail.com>
2020-12-05 18:39:17 -0300

    Adding Blocks

---
+++ Jogo/Assets/scripts/Block.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    public SpriteRenderer sprR;
+    public Sprite duzentos_reais;
+    public Sprite cinco_reais;
+    public Sprite dois_reais;
+    public Sprite dez_reais;
+    public Sprite cinquenta_reais;
+    public Sprite cem_reais;
+    public Sprite vinte_reais;
+
+    public int value;
+
+    public int count;
+    public bool next;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sprR = GetComponent<SpriteRenderer>();
+        next = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(next){
+            NextSprite();
+            next = false;
+        }
+    }
+
+    void NextSprite()
+    {
+        count = Random.Range(0,7);
+        if(count == 0){
+            sprR.sprite = duzentos_reais;
+            value = 200;
+        }else if(count == 1){
+            sprR.sprite = cinco_reais;
+            value = 5;
+        }else if(count == 2){
+            sprR.sprite = dez_reais;
+            value = 10;
+        }else if(count == 3){
+            sprR.sprite = cem_reais;
+            value = 100;
+        }else if(count == 4){
+            sprR.sprite = cinquenta_reais;
+            value = 50;
+        }else if(count == 5){
+            sprR.sprite = vinte_reais;
+            value = 20;
+        }else{
+            sprR.sprite = dois_reais;
+            value = 2;
+        }
+    }
+    //TODO: Fix the collision with player
+    void OnTriggerEnter2D(Collider2D other){
+        //other.GetComponent<Player>().takeDamage(value);
+        //GetComponent<Transform>().GameObject.SetActive(false);
+        Debug.Log("Tocou no valor: "+value);
+    }
+}
--- Jogo/Assets/scripts/Challenge.cs
+++ Jogo/Assets/scripts/Challenge.cs
@@ -12,11 +12,18 @@ public class Challenge : MonoBehaviour
     public GameObject spr2;
     public GameObject spr3;
 
+    private Block block1;
+    private Block block2;
+    private Block block3;
+
     private Transform tr;
     // Start is called before the first frame update
     void Start()
     {
         tr = GetComponent<Transform>();
+        block1 = spr1.GetComponent<Block>();
+        block2 = spr2.GetComponent<Block>();
+        block3 = spr3.GetComponent<Block>();
         show = 0;
         Restart();
     }
@@ -26,26 +33,59 @@ public class Challenge : MonoBehaviour
     {
         tr.position = tr.position - (tr.right*speed);
         if(tr.position.x <= -9.5f){
-            show++;
             Restart();
         }
     }
 
     void Restart(){
         tr.position = new Vector2(9.5f,0);
+        DeactiveAll();
+        show++;
+        ActiveThisCase();
+    }
+
+    void DeactiveAll(){
         spr1.SetActive(false);
         spr2.SetActive(false);
         spr3.SetActive(false);
-        if(show % 3 == 0){
+    }
+
+    void ActiveAll(){
+        spr1.SetActive(true);
+        spr2.SetActive(true);
+        spr3.SetActive(true);
+    }
+
+    void ActiveThisCase(){
+        if(show % 7 == 0){
             spr1.SetActive(true);
-        }else if(show % 3== 1){
+            block1.next = true;
+        }else if(show % 7 == 1){
             spr2.SetActive(true);
-        }else if(show % 3 == 2){
+            block2.next = true;
+        }else if(show % 7 == 2){
             spr3.SetActive(true);
+            block3.next = true;
+        }else if(show % 7 == 3){
+            spr1.SetActive(true);
+            block1.next = true;
+            spr2.SetActive(true);
+            block2.next = true;
+        }else if(show % 7 == 4){
+            spr1.SetActive(true);
+            block1.next = true;
+            spr3.SetActive(true);
+            block3.next = true;
+        }else if(show % 7 == 5){
+            spr2.SetActive(true);
+            block2.next = true;
+            spr3.SetActive(true);
+            block3.next = true;
         }else{
-            spr1.SetActive(false);
-            spr2.SetActive(false);
-            spr3.SetActive(false);
+            spr2.SetActive(true);
+            block2.next = true;
+            spr3.SetActive(true);
+            block3.next = true;
         }
     }
 }
